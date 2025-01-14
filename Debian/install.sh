4144522177e23e711b4172207f9a4bb7697df065
DunamisMax <dunamismax@tutamail.com>
2025-01-14 02:38:29 -0500

    gi

--- Debian/install.sh
+++ Debian/install.sh
@@ -1062,8 +1062,11 @@ install_dev_build_deps() {
 install_caddy() {
   log "Installing and enabling Caddy..."
 apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' \
+  | sudo gpg --batch --yes --dearmor \
+       -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' \
+  | tee /etc/apt/sources.list.d/caddy-stable.list
 apt update -y
 apt install -y caddy
 systemctl enable caddy
