9b7cecaa0892131bbdbc8d7e2a16d849d6c4d4d6
DunamisMax <dunamismax@tutamail.com>
2025-01-14 14:24:45 -0500

    modified:   Debian/install.sh

--- Debian/install.sh
+++ Debian/install.sh
@@ -942,8 +942,7 @@ install_apt_dependencies() {
         apt-transport-https \
         gnupg \
         lsb-release \
-        jq \
-        nginx
+        jq
 
     # Optionally remove automatically installed packages no longer needed
     sudo apt-get autoremove -y
