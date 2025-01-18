62ef5f531d225cdf91d48fbcedfe072b26cd5baa
dunamismax <dunamismax@tutamail.com>
2025-01-18 08:24:50 -0500

    Update backup_dotfiles_daemon.sh

--- misc-configs/i3/backup_dotfiles_daemon.sh
+++ misc-configs/i3/backup_dotfiles_daemon.sh
@@ -2,6 +2,6 @@
 
 # Run the backup script every minute
 while true; do
-    ~/.config/i3/backup_dotfiles.sh >> ~/.config/i3/backup_log.txt 2>&1
+    ~/.local/bin/backup_dotfiles.sh >> ~/.config/i3/backup_log.txt 2>&1
     sleep 60
 done
